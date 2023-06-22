27f332df13242077bffc95de6e8d9bd8dd1b23c5
dtorresc8 <124192927+dtorresc8@users.noreply.github.com>
2023-06-22 10:43:04 -0600

    Login Page Component

---
+++ src/auth/pages/LoginPage.jsx
@@ -0,0 +1,53 @@
+import { Button, Grid, TextField, Typography, Link } from "@mui/material";
+import { Google } from "@mui/icons-material";
+import { Link as RouterLink } from "react-router-dom";
+import { AuthLayout } from "../layout/AuthLayout";
+
+export const LoginPage = () => {
+  return (
+    <AuthLayout title="Login">
+      <form action="">
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Email"
+              type="email"
+              placeholder="email@google.com"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Password"
+              type="password"
+              placeholder="password"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={12} sm={6}>
+              <Button variant="contained" fullWidth>
+                Login
+              </Button>
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <Button variant="contained" fullWidth>
+                <Google />
+                <Typography sx={{ ml: 1 }}>Google</Typography>
+              </Button>
+            </Grid>
+          </Grid>
+
+          <Grid container direction="row" justifyContent="end">
+            <Link component={RouterLink} color="inherit" to="/auth/register">
+              Register
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
+  );
+};
